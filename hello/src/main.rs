6618f3e446c8384f01958c664d661253574d8ef6
syf20020816 <syf20020816@outlook.com>
2023-09-13 12:12:16 +0800

    20230913

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,6 +1,13 @@
-
 slint::include_modules!();
 
-fn main() {
-    MainWindow::new().unwrap().run().unwrap();
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    let ui_handle = ui.as_weak();
+    ui.on_request_increase_value(move || {
+        let ui = ui_handle.unwrap();
+        ui.set_counter(ui.get_counter() + 1);
+    });
+
+    ui.run()
 }
